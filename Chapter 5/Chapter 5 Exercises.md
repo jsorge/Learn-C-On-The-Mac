aa04c8339a3082b0896cedb21544faec0f7cf5f2
Jared <jared@jsorge.net>
2013-07-27 21:09:11 -0700

    Completed chapter 5

---
+++ Chapter 5/Chapter 5 Exercises.md
@@ -0,0 +1,9 @@
+# Learn C on the Mac Exercises
+## Chapter 5
+
+* What was the total number of times the debugger stopped the program?
+	* 4
+* How many times did the debugger stop before the printf() statement?
+	* 3
+* Can you explain why?
+	* It stopped every time the the SayHello function was called, which was 3 as well as the extra time in main when it called SayHello
---
+++ Chapter 5/Operators/Operators/main.c
@@ -0,0 +1,29 @@
+//
+//  main.c
+//  Operators
+//
+//  Created by Jared Sorge on 7/27/13.
+//  Copyright (c) 2013 Jared Sorge. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+    int myInt;
+    
+    myInt = 3 * 2;
+    
+    myInt += 1;
+    
+    myInt -= 5;
+    
+    myInt *= 10;
+    
+    myInt /= 4;
+    
+    myInt /= 2; 
+    
+    return 0;
+}
+
